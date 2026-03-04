643003268b30d454a071b1336deefa767e38c98a
Luck-Css <lucascharff@proton.me>
2026-03-04 17:22:36 -0300

    small edits

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 function GameCard() {
     const games = ["Resident evil requiem", "Resident evil 4 remake", "Resident evil 2 remake"];
 
+    function selectGame() {
+      alert("Funciona")
+    }
+
   return (
-    <div className='grid font-rovian text-xl text-zinc-800 place-items-center'>
-       <h2>Choose a game:</h2> 
-        <select className='place-items-center'>
-            <option value="Resident evil requiem">Resident evil requiem</option>
-            <option value="Resident evil 4 remake">Resident evil 4 remake</option>
-            <option value="Resident evil 2 remake">Resident evil 2 remake</option>
+    <div className='grid text-xl text-zinc-800 place-items-center'>
+      <label for="game-select">Choose a Game:</label>
+       <div className=''>
+        <select name="gameSelector" id="game-select" className='block w-full px-4 py-2.5 bg-neutral-secondary-medium border border-default-medium text-heading rounded-2xl focus:ring-brand focus:border-brand shadow-xs placeholder:text-body'>
+          <option value="Resident-evil4-re">Resident evil 4 Remake</option>
+          <option value="Resident-evil2-re">Resident evil 2 Remake</option>
+          <option value="Resident-requiem">Resident evil 9 Requiem</option>
+
         </select>
+        
+
+       </div>
     </div>
   )
 }
